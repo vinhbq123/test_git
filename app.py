cbfa70e354b59b4ff6089b6aed4828ccc7c25fd4
Quang Vinh <zeimier@gmail.com>
2021-05-24 22:33:44 +0700

    add files

---
+++ app.py
@@ -0,0 +1,11 @@
+""" Main """
+
+from flask import Flask, app
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello!"
+
+if __name__ == "__main__":
+    app.run(host ='0.0.0.0')
